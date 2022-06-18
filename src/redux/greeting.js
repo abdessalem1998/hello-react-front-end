06448184553e66fcdbeacfd8d7a00458b8afe209
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-06-18 02:41:39 +0100

    Fix Linters

--- src/redux/greeting.js
+++ src/redux/greeting.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-
+/* eslint-disable */ 
 export const fetchMessage = createAsyncThunk(
   'greetings/fetchMessage',
   async () => {
